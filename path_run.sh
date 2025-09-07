c9c728043a9b56022cc8010f07181992ae0aa664
Fraser Mince <frasermince@gmail.com>
2025-09-07 12:36:52 -0600

    Misc changes.

--- path_run.sh
+++ path_run.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
-for seed in 0 1 2 3 4 5; do
+for seed in 0 1 2 3 4; do
   for lr in 1e-3 5e-4 1e-4 5e-5 1e-5; do
     for depth in 2 3; do
-      for width in 8 16 32; do
+      # for width in 2 4 8 16 32; do
+      for width in 64; do
         for opt in "generate_optimal_path=true" "generate_optimal_path=false"; do
           # Create  capacity array of length $depth, all elements $width
           capacity=$(printf "[%s]\n" "$(yes $width | head -n $depth | paste -sd, -)")
